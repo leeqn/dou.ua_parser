948d584c342376eb0c2edd65769c81015176b5d1
leeqn <holygamenoname@gmail.com>
2026-06-03 00:01:25 +0200

    raw_teXt

--- dou.py
+++ dou.py
@@ -1,6 +1,5 @@
 import requests
 import sqlite3
-import pandas as pd
 from bs4 import BeautifulSoup
 
 url = "https://jobs.dou.ua/first-job/"
@@ -9,7 +8,7 @@ cursor = connection.cursor()
 
 cursor.execute('''CREATE TABLE IF NOT EXISTS vacancies (id INTEGER PRIMARY KEY AUTOINCREMENT,
                   Title TEXT Not NULL,
-                  Link TEXT Not NULL,,
+                  Link TEXT Not NULL,
                   Raw_Text TEXT)''')
 connection.commit()
 
@@ -18,30 +17,29 @@ headers = {
     "Accept-Language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7"
 }
 
-response = requests.get(url, headers=headers)
+#response = requests.get(url, headers=headers)
+
+with requests.Session() as session:
+    response=session.get(url, headers=headers)
+
 if response.status_code == 200:
 
     soup = BeautifulSoup(response.text, 'html.parser')
     vacancy_links = soup.find_all('a', class_='vt')
 
-    title_list=[]
-    href_list=[]
-    raw_list=[]
-
     for link in vacancy_links:
-        temp_url = link.get('href')
-        temp_response = requests.get(temp_url, headers=headers)
-        temp_soup = BeautifulSoup(temp_response.text, 'html.parser')
-        raw=temp_soup.find('div', class_='b-typo')
-
-        raw_text=raw.get_text()
         title= link.text.strip()
         href= link.get('href')
 
-        cursor.execute('SELECT id FROM vacancies WHERE link = ?', (href,))
-        title_list.append(title)
-        href_list.append(href)
+        temp_url=href
+        temp_response=session.get(temp_url, headers=headers)
+        temp_soup=BeautifulSoup(response.text, 'html.parser')
+        raw_text = temp_soup.find('div', class_='b-typo')
 
-    vacancies = pd.DataFrame({'Title': title_list, 'Link': href_list, 'Raw': raw_list})
-    print(vacancies.head())
+        cursor.execute(("INSERT INTO vacancies (title,link,raw_text) VALUES (?,?,?)"),
+        (title,href,raw_text))
+        connection.commit()
+        print(f"Saved {title}")
+connection.close()
+print("DONE")
 
--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from dou import vacancies
+import pandas as pd
+from sqlite3 import connect
+conn=connect('jobs.db')
+jobs=pd.read_sql('SELECT id,Title,Link,Raw_Text from vacancies', conn)
 
-print(vacancies.head())
+print(jobs.head())
 
