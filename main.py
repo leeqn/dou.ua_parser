496ec742377f3a8cb7e2ab6c9e2daa1a52cd6bb3
leeqn <holygamenoname@gmail.com>
2026-06-09 17:33:45 +0200

    postgres

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 import pandas as pd
+from website import get_info
+from sort import skill_filter
 from sqlite3 import connect
+
+def delete_table():
+    conn=connect('jobs.db')
+    cursor=conn.cursor()
+    cursor.execute("DELETE FROM vacancies")
+    cursor.execute("DELETE FROM sqlite_sequence WHERE name='vacancies'")
+    conn.commit()
+    conn.close()
+
+#interface
+url = "https://jobs.dou.ua/first-job/"
+
+get_info(url)
+
 conn=connect('jobs.db')
-jobs=pd.read_sql('SELECT id,Title,Link,Raw_Text from vacancies', conn)
-
+jobs=pd.read_sql('SELECT id,Title,Link,Raw_Text,Company from vacancies', conn, index_col='id')
+jobs['Found_Skills'] = jobs['Raw_Text'].apply(skill_filter)
 print(jobs.head())
-
+jobs.to_csv('jobs.csv')
--- website.py
+++ website.py
@@ -6,7 +6,7 @@ def get_info(url):
     connection = sqlite3.connect('jobs.db')
     cursor = connection.cursor()
 
-    cursor.execute("DROP TABLE IF EXISTS vacancies")
+#cursor.execute("DROP TABLE IF EXISTS vacancies")
 
     cursor.execute('''CREATE TABLE IF NOT EXISTS vacancies (id INTEGER PRIMARY KEY AUTOINCREMENT,
                       Title TEXT Not NULL,
@@ -32,22 +32,22 @@ def get_info(url):
         company_name=soup.find('.b-compinfo .l-n a')
 
         for link in vacancy_links:
-            title= link.text.strip()
-            href= link.get('href')
+            if link:
+                title= link.text.strip()
+                href= link.get('href')
 
-            temp_url=link.get('href')
-            temp_response=requests.get(temp_url, headers=headers)
-            temp_soup=BeautifulSoup(temp_response.text, 'html.parser')
-            raw = temp_soup.find('div', class_='b-typo')
-            raw_text = raw.get_text()
-            l_n = temp_soup.find('div', class_='l-n')
-            company_name=l_n.find('a')
-            company = company_name.text.strip()
+                temp_url=link.get('href')
+                temp_response=requests.get(temp_url, headers=headers)
+                temp_soup=BeautifulSoup(temp_response.text, 'html.parser')
+                raw = temp_soup.find('div', class_='b-typo')
+                raw_text = raw.get_text() if raw else 'None'
+                l_n = temp_soup.find('div', class_='l-n')
+                company_name=l_n.find('a')
+                company = company_name.text.strip() if company_name else 'None'
 
-            cursor.execute(("INSERT INTO vacancies (title,company,link,raw_text) VALUES (?,?,?,?)"),
-            (title,company,href,raw_text))
-            connection.commit()
-            #print(f"Saved {raw_text}")
+                cursor.execute(("INSERT INTO vacancies (title,company,link,raw_text) VALUES (?,?,?,?)"),
+                (title,company,href,raw_text))
+                connection.commit()
+                #print(f"Saved {raw_text}")
     connection.close()
     print("DONE")
-
